eaf2846a3620a851de8a98036e5533752b29954d
Ayushi Indani <ayushi.indani@gmail.com>
2020-09-27 15:37:08 +0530

    Add files via upload

---
+++ HRManagement/src/hrmanagement/hr_login.java
@@ -0,0 +1,284 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hrmanagement;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Ayushi
+ */
+public class hr_login extends javax.swing.JFrame {
+    int count=0;
+    public hr_login() {
+        initComponents();
+    }
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jPasswordField1 = new javax.swing.JPasswordField();
+        jButton1 = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+        jPasswordField2 = new javax.swing.JPasswordField();
+        jButton2 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jPanel1MouseClicked(evt);
+            }
+        });
+
+        jLabel1.setText("HR UserID");
+
+        jLabel2.setText("Pasword");
+
+        jButton1.setText("Login");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(33, 33, 33)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(31, 31, 31)
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jButton1)
+                            .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(45, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(90, 90, 90)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(29, 29, 29)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(26, 26, 26)
+                .addComponent(jButton1)
+                .addContainerGap(169, Short.MAX_VALUE))
+        );
+
+        jLabel3.setText("Employee ID");
+
+        jLabel4.setText("Password");
+
+        jButton2.setText("Login");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(34, 34, 34)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel4))
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(11, 11, 11)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jButton2)
+                            .addComponent(jPasswordField2, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(59, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(87, 87, 87)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(29, 29, 29)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(jPasswordField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(26, 26, 26)
+                .addComponent(jButton2)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        String id=jTextField1.getText();
+        String pwd=jPasswordField1.getText();
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/hrmanagement","root","root");
+            PreparedStatement ps=con.prepareStatement("select * from hr where hr_id=? and password=?");
+            ps.setString(1, id);
+            ps.setString(2,pwd);
+            ResultSet rs=ps.executeQuery();      
+            int x=0;
+            if(rs.next())
+            {
+                x=1;
+                JOptionPane.showMessageDialog(this,"Login Success");
+                dispose();
+                new hr_home(jTextField1.getText()).setVisible(true);
+            }
+            else
+                JOptionPane.showMessageDialog(this,"Login Failed");
+          }catch(Exception e)
+            {
+            JOptionPane.showMessageDialog(this,e);
+            }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jPanel1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel1MouseClicked
+        count++;
+        if(count==3)
+        {
+            new adminlog().setVisible(true);
+        }
+    }//GEN-LAST:event_jPanel1MouseClicked
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        String id=jTextField2.getText();
+        String pwd=jPasswordField2.getText();
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/hrmanagement","root","root");
+            PreparedStatement ps=con.prepareStatement("select * from emp where e_id=? and password=?");
+            ps.setString(1,id);
+            ps.setString(2,pwd);
+            ResultSet rs=ps.executeQuery();      
+            int x=0;
+            if(rs.next())
+            {
+                x=1;
+                JOptionPane.showMessageDialog(this,"Login Success");
+                dispose();
+//                String sql="SELECT SYSDATE()";
+//                ResultSet rs1=ps.executeQuery(sql);
+//                rs1.next();
+//                Date d=rs1.getDate(1);
+                Date d1=new Date(System.currentTimeMillis());
+                SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                String d=s.format(d1);
+                new emp_home(jTextField2.getText(),d).setVisible(true);
+            }
+            else
+                JOptionPane.showMessageDialog(this,"Login Failed");
+          }catch(Exception e)
+            {
+            JOptionPane.showMessageDialog(this,e);
+            }
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(hr_login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(hr_login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(hr_login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(hr_login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new hr_login().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPasswordField jPasswordField1;
+    private javax.swing.JPasswordField jPasswordField2;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ HRManagement/test.sql
@@ -0,0 +1,105 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.0.24-community-nt : Database - hrmanagement
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`hrmanagement` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `hrmanagement`;
+
+/*Table structure for table `e_attendence` */
+
+DROP TABLE IF EXISTS `e_attendence`;
+
+CREATE TABLE `e_attendence` (
+  `e_id` varchar(20) NOT NULL,
+  `projectname` varchar(20) default NULL,
+  `login` datetime default NULL,
+  `logout` datetime default NULL,
+  `status` tinyint(1) default NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `e_attendence` */
+
+insert  into `e_attendence`(`e_id`,`projectname`,`login`,`logout`,`status`) values ('ayu','sdsas','2016-10-11 05:26:19','2016-10-11 05:26:22',0),('a1','fdsfsd','2016-10-11 05:28:12','2016-10-11 05:28:17',0),('b1','dsf','2016-10-11 05:30:02','2016-10-11 05:30:06',0),('c1','kjhk','2016-10-11 05:32:03','2016-10-11 05:32:20',0),('ayu','ffsdf','2016-10-11 05:53:40','2016-10-11 05:54:29',0),('a1','hgh','2016-10-12 06:15:40','2016-10-12 06:16:11',0),('a1','jkg','2016-10-12 06:16:40','2016-10-12 06:17:38',0),('a1','trt','2016-10-12 06:19:10','2016-10-12 06:20:02',0),('ayu','dfs','2016-10-12 10:54:07','2016-10-12 10:55:25',0),('a1','sda','2016-10-12 10:57:33','2016-10-12 10:57:48',0),('a1','gf','2016-10-12 11:04:01','2016-10-12 11:04:10',0),('ayu',NULL,'2016-10-12 11:12:20','2016-10-12 11:12:33',0),('ayu','dfs','2016-10-12 11:16:17','2016-10-12 11:17:08',0);
+
+/*Table structure for table `e_mgmt` */
+
+DROP TABLE IF EXISTS `e_mgmt`;
+
+CREATE TABLE `e_mgmt` (
+  `hr_id` varchar(10) NOT NULL default '',
+  `to1` varchar(10) default NULL,
+  `sub` varchar(50) default NULL,
+  `msg` varchar(50) default NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `e_mgmt` */
+
+insert  into `e_mgmt`(`hr_id`,`to1`,`sub`,`msg`) values ('c1','a1','asf','aff'),('c1','ayu','fds','dgdggdfg');
+
+/*Table structure for table `emp` */
+
+DROP TABLE IF EXISTS `emp`;
+
+CREATE TABLE `emp` (
+  `e_id` varchar(20) NOT NULL,
+  `password` varchar(20) default NULL,
+  `name` varchar(20) default NULL,
+  `email` varchar(50) default NULL,
+  `dept` varchar(20) default NULL,
+  `address` varchar(20) default NULL,
+  `mobile` varchar(20) default NULL,
+  PRIMARY KEY  (`e_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `emp` */
+
+insert  into `emp`(`e_id`,`password`,`name`,`email`,`dept`,`address`,`mobile`) values ('a1','612','asw','agf','ahgf','afg','966664486'),('am','108','amit','asasas','it','gsgsg','4524'),('ayu','933','ayushi indani','ayushi','it','xyz','94641'),('ayushi','035','ayushi','ayushi.indani','cs','xyz','99999999'),('b1','148','b','b','b','b','98648896'),('c1','845','c','c','c','c','986485136');
+
+/*Table structure for table `hr` */
+
+DROP TABLE IF EXISTS `hr`;
+
+CREATE TABLE `hr` (
+  `name` varchar(20) NOT NULL,
+  `address` varchar(50) default NULL,
+  `email` varchar(30) default NULL,
+  `zip` decimal(10,0) default NULL,
+  `mobile` decimal(12,0) default NULL,
+  `hr_id` varchar(20) default NULL,
+  `password` varchar(20) default NULL,
+  PRIMARY KEY  (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `hr` */
+
+insert  into `hr`(`name`,`address`,`email`,`zip`,`mobile`,`hr_id`,`password`) values ('a','a','a@a.com','454855','914541512','a1','12308'),('b','bb','b@b.com','452','77777','b1','66668'),('c','c','c@c.com','452154','9415345221','c1','561'),('d','d','d@d.com','451425','9435122132','d1','119');
+
+/*Table structure for table `lea` */
+
+DROP TABLE IF EXISTS `lea`;
+
+CREATE TABLE `lea` (
+  `e_id` varchar(20) NOT NULL,
+  `desc_leave` varchar(50) default NULL,
+  `status_leave` varchar(20) default NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `lea` */
+
+insert  into `lea`(`e_id`,`desc_leave`,`status_leave`) values ('ayu','fsdf','Approved'),('a1','sdc','Rejected'),('a1','gdfg','Rejected'),('ayu','fdf','NA');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
